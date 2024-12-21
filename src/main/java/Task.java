23786fe6a400db7c096d6e744f12b3e634f415c0
vTyzee <emil.munassypov@ivkhk.ee>
2024-12-21 15:22:30 +0200

    first commit

---
+++ src/main/java/Task.java
@@ -0,0 +1,55 @@
+public class Task {
+    int id;
+    String name;
+    String description;
+    Status status;
+
+    public Task(String description, int id, String name, Status status) {
+        this.description = description;
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
